dd23fb6ffa08cfd6d2a5d8b85113e215d7110b6e
Your name <Your email>
2023-06-23 11:59:17 +0200

    backend

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/year', function (req, res) {
+  const date = new Date().getFullYear().toString;
+  res.json({ year: date });
+ });
+
+
 module.exports = router;
