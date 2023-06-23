bb16ae1eadfcea621d5a2e7edc3dba512a095984
Your name <Your email>
2023-06-23 11:48:07 +0200

    backend

--- routes/year.js
+++ routes/year.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    const date = new Date();
-    res.json({ now: date });
+    const date = new Date().getFullYear().toString;
+    res.json({ year: date });
    });
   
   module.exports = router;
